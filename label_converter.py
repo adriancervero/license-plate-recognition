4c76cf81fa39cb629c5dae5b91d81c3db416d302
Adrián Cerveró <aceor93@gmail.com>
2021-06-19 17:50:17 +0200

    script to convert labels to yolo format

---
+++ label_converter.py
@@ -0,0 +1,81 @@
+import numpy as np
+from xml.dom.minidom import parse
+import os
+
+ANNOTATIONS_PATH = 'data/annotations'
+DATA_ROOT = "data/"
+DEST_LABELS_PATH = 'labels'
+
+def cord_converter(size, box):
+    """
+    convert xml annotation to darknet format coordinates
+    :param size： [w,h]
+    :param box: anchor box coordinates [upper-left x,uppler-left y,lower-right x, lower-right y]
+    :return: converted [x,y,w,h]
+    """
+    x1 = int(box[0])
+    y1 = int(box[1])
+    x2 = int(box[2])
+    y2 = int(box[3])
+
+    dw = np.float32(1. / int(size[0]))
+    dh = np.float32(1. / int(size[1]))
+
+    w = x2 - x1
+    h = y2 - y1
+    x = x1 + (w / 2)
+    y = y1 + (h / 2)
+
+    x = x * dw
+    w = w * dw
+    y = y * dh
+    h = h * dh
+    return [x, y, w, h]
+    
+def save_file(img_jpg_file_name, size, img_box):
+    classes = ['license']
+    save_file_name = DATA_ROOT + DEST_LABELS_PATH + '/' + img_jpg_file_name + '.txt'
+    print(save_file_name)
+    file_path = open(save_file_name, "a+")
+    for box in img_box:                  
+        #cls_num = classes.index(box[0]) # find class_id
+        cls_num = 0
+        new_box = cord_converter(size, box[1:]) # convert box coord into YOLO x,y,w,h
+
+        file_path.write(f"{cls_num} {new_box[0]} {new_box[1]} {new_box[2]} {new_box[3]}\n")
+
+    file_path.flush()
+    file_path.close()
+    
+def get_xml_data(file_path, img_xml_file):
+    img_path = file_path + '/' + img_xml_file + '.xml'
+    print(img_path)
+
+    dom = parse(img_path)
+    root = dom.documentElement
+    img_name = root.getElementsByTagName("filename")[0].childNodes[0].data
+    img_size = root.getElementsByTagName("size")[0]
+    objects = root.getElementsByTagName("object")
+    img_w = img_size.getElementsByTagName("width")[0].childNodes[0].data
+    img_h = img_size.getElementsByTagName("height")[0].childNodes[0].data
+    img_c = img_size.getElementsByTagName("depth")[0].childNodes[0].data
+    # print("img_name:", img_name)
+    # print("image_info:(w,h,c)", img_w, img_h, img_c)
+    img_box = []
+    for box in objects:
+        cls_name = box.getElementsByTagName("name")[0].childNodes[0].data
+        x1 = int(box.getElementsByTagName("xmin")[0].childNodes[0].data)
+        y1 = int(box.getElementsByTagName("ymin")[0].childNodes[0].data)
+        x2 = int(box.getElementsByTagName("xmax")[0].childNodes[0].data)
+        y2 = int(box.getElementsByTagName("ymax")[0].childNodes[0].data)
+        img_jpg_file_name = img_xml_file + '.jpg'
+        img_box.append([cls_name, x1, y1, x2, y2])
+    # print(img_box)
+    # test_dataset_box_feature(img_jpg_file_name, img_box)
+    save_file(img_xml_file, [img_w, img_h], img_box)
+
+if __name__ == "__main__":
+    files = os.listdir(ANNOTATIONS_PATH)
+    for file in files:
+        file_xml = file.split('.')
+        get_xml_data(ANNOTATIONS_PATH, file_xml[0])
